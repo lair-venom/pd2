84b71126ba0ce48d645752a39cd88ae602f3af11
lair-venom <mir.devil50@gmail.com>
2025-12-26 17:29:20 +0300

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: process.env.GITHUB_PAGES ? '/payday2-randomizer/' : './',
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+});
